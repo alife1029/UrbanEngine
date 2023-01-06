6b98ffc16a0ce7a44b5754e3ce4ad6e5f751b53f
Ali Efe GÜR <alife1029@gmail.com>
2023-01-07 01:47:01 +0300

    Lifecycle Print Outs Removed

--- TestApp/src/TestApp.cpp
+++ TestApp/src/TestApp.cpp
@@ -6,25 +6,20 @@ using namespace UrbanEngine;
 
 TestApp::TestApp() : App()
 {
-	std::cout << "TestApp::TestApp()" << std::endl;
 }
 
 TestApp::~TestApp()
 {
-	std::cout << "TestApp::~TestApp()" << std::endl;
 }
 
 void TestApp::Start()
 {
 	App::Start();
-	std::cout << "TestApp::Start()" << std::endl;
 }
 
 void TestApp::Update()
 {
 	App::Update();
-	std::cout << "TestApp::Update()" << std::endl;
-	//AppManager::QuitApplication();
 	m_Window->ProcessEvents();
 	m_Window->Gfx()->ClearBuffer(1.0f, 0.0f, 0.0f, 1.0f);
 	m_Window->Gfx()->EndFrame();
